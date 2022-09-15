58026f7b90b1bf2c7bf4c481256732f0ddad45eb
Kevin Wong <kevinzw8@gmail.com>
2022-09-15 12:32:38 -0700

    redditScraperOOP edits

--- scrapeRedditOOP.py
+++ scrapeRedditOOP.py
@@ -1,12 +1,14 @@
 
 
-class Person:
+class ScrapReddit:
     import praw
     def __init__(self):
         self.reddit_read_only = self.praw.Reddit(client_id="1LUhfALD3uJgbBeFVFMOGQ",         # your client id
                                     client_secret="46QJC2mttPTyOY7DwzlOdkP1nxYNqw",      # your client secret
                                     user_agent="Rbot")        # your user agent
-
+    def readSub(self, SubReddit):
+        self.subreddit = self.reddit_read_only.subreddit(SubReddit)
+        return self.subreddit
     def removePost(postList, target):
         returnIndexs = []
         
@@ -30,17 +32,12 @@ class Person:
         for i in returnIndexs:
             returnValues.append(postList[i])
         return returnValues
-        
-    def readSub(self, SubReddit):
-        return self.reddit_read_only.subreddit(SubReddit)
-p1 = Person("John", 36)
-
-print(p1.name)
-print(p1.age)
 
 
 
 
+post = ScrapReddit()
+subreddit = post.readSub("TIFU")
  
 # Display the name of the Subreddit
 print("Display Name:", subreddit.display_name)
@@ -59,6 +56,6 @@ for post in subreddit.hot(limit=5):
     postList.append(data)
 
 #postList = [["[Mod Post] post0", "post0 content"],["post1", "post1 content"],["post2", "post2 content"],["post3", "post3 content"],["post4", "post4 content"]]
-postList = removePost(postList, "[Mod Post]")
-postList = removePost(postList, "[Breaking News]")
+postList = post.removePost(postList, "[Mod Post]")
+postList = post.removePost(postList, "[Breaking News]")
 print(postList)
